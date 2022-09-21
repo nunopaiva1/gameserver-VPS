50d23043860c539c3145fd180e1bab6f09b5a163
nunopaiva1 <nunopaiva_1@hotmail.com>
2022-09-21 16:35:24 +0000

    script v3

--- gsVPS-transfer.sh
+++ gsVPS-transfer.sh
@@ -2,19 +2,17 @@
 
 DIR="/pro"
 
-command -v git >/dev/null 2>&1 ||
-{ echo >&2 "Git is not installed. Installing..";
-  sudo apt-get git
-}
-
 #The next 3 lines replace the ssh config file. A new one is added that enables the SSH on a VPS.
 #Finally, we enable this service and restart it to ensure it will be turned on by default.
 
-rm /etc/ssh/sshd_config
-cp sshd_config /etc/ssh
+rm /etc/ssh/sshd_config -r
+cp sshd_config /etc/ssh ;
 
-systemctl enable ssh && systemctl restart ssh &> /dev/null ;
+systemctl enable ssh && systemctl restart ssh &> /dev/null
 
+echo ""
+echo "SSH has been successfully activated."
+echo ""
 
 echo ""
 echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -49,7 +47,6 @@ if [ -d "$DIR" ]; then
 else
   ###  Control will jump here if $DIR does NOT exists ###
   cd / && mkdir pro && cd /pro
-  exit 1
 fi
 
 mkdir $SERVICENAME && cd $SERVICENAME ;
